4d5f0ba7e7fd8322afadee94580768e1c00e94db
Jon Suphammer <jon@suphammer.net>
2020-09-04 09:21:40 +0200

    Added arm toolchain. For now it's precomiled for x86 host

---
+++ arm-none-eabi-gcc.rb
@@ -0,0 +1,13 @@
+class ArmNoneEabiGcc < Formula
+  desc "GCC for embedded ARM processors"
+  homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
+  version '9-2019-q4-major'
+
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/RC2.1/gcc-arm-none-eabi-9-2019-q4-major-mac.tar.bz2'
+  sha256 '1249f860d4155d9c3ba8f30c19e7a88c5047923cea17e0d08e633f12408f01f0'
+
+  def install
+    (prefix/"gcc").install Dir["./*"]
+    Dir.glob(prefix/"gcc/bin/*") { |file| bin.install_symlink file }
+  end
+end
